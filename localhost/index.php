ed86016f239ccce52927402d7c375a15d10381e5
User1890 <52070784+User1890@users.noreply.github.com>
2021-11-28 15:19:19 +0300

    Update index.php

--- localhost/index.php
+++ localhost/index.php
@@ -3,7 +3,7 @@
 
   if (!isset($_SESSION['username'])) {
     $_SESSION['msg'] = "You must log in first";
-    header('location: ../registration/login.php');
+    header('location: ../registration/register.php');
   }
   if (isset($_GET['logout'])) {
     session_destroy();
